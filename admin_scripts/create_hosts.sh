bb0e4bef4b1ea0db93661ab29865865a324a3485
Nilesh Attarde <nsa5953@gmail.com>
2020-01-07 18:11:40 +0530

    Update create_host.sh to get container details and update hosts file

--- admin_scripts/create_hosts.sh
+++ admin_scripts/create_hosts.sh
@@ -4,19 +4,38 @@
 #
 #
 path= $PWD
-echo "This Script will create host file from your running container for your ansible testing"
+echo -e "This Script will Display docker container details and create host file from your running container for your ansible testing\n"
+#
+#
+#
+__displayDETAILS (){
+echo -e "* * * * * Docker Container Details * * * * *" 
+for IPADDR in  `docker ps -a | awk '{print $1}' | tail -n +2`
+   do
+    getinfo=`docker inspect -f '{{.Name }} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IPADDR | tr -d "/"`
+    echo -e "\t$getinfo"
+   done;
+}
+
+#
+#
+#
+
 __fetchIPAddress (){
 for IPADDR in  `docker ps -a | awk '{print $1}' | tail -n +2`
    do
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IPADDR
    done;
-
 }
+
+
 
 # creating hosts file
 cat > ../"$path"/hosts << EOF
 [docker_group]
 EOF
 
+
 # Calling function
+__displayDETAILS
 __fetchIPAddress >> ../"$path"/hosts
