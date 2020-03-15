6fdaec35d26a964c5be772411259094b92e666de
Nilesh Attarde <nsa5953@gmail.com>
2020-03-15 13:32:12 +0530

    case and selections menu for create_hosts.sh

--- system-utilities/create_hosts.sh
+++ system-utilities/create_hosts.sh
@@ -7,7 +7,6 @@ path= $PWD
 echo -e "This Script will Display docker container details and create host file from your running container for your ansible testing\n"
 #
 #
-#
 __displayDETAILS (){
 echo -e "* * * * * Docker Container Details * * * * *" 
 for IPADDR in  `docker ps -a | awk '{print $1}' | tail -n +2`
@@ -16,12 +15,9 @@ for IPADDR in  `docker ps -a | awk '{print $1}' | tail -n +2`
     echo -e "\t$getinfo"
    done;
 }
-
 #
 #
-#
-
-__fetchIPAddress (){
+__updateHosts (){
 
 # creating hosts file
 cat > ../"$path"/hosts << EOF
@@ -33,12 +29,19 @@ for IPADDR in  `docker ps -a | awk '{print $1}' | tail -n +2`
    do
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IPADDR
    done;
+
 }
 
-
-
-
-
-# Calling function
-__displayDETAILS
-#__fetchIPAddress >> ../"$path"/hosts
+# use case statement to make decision for rental
+option=$1
+select option in "List containers" "update hosts" "Quit"
+do
+   case $option in
+      "List containers") __displayDETAILS;;
+      "update hosts") echo -e "`__updateHosts >> ../"$path"/hosts` Hosts updated"
+                     exit 1;;
+      "Quit") exit 1;;
+      *)  
+      echo "`basename ${0}`:usage: [1- List container IP details ] | [2 - Update hosts file]" 
+   esac
+done
